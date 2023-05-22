6a7055c0ca403ed38e8a0689e59873f233a8edd8
eric <15854168262@qq.com>
2023-05-22 19:24:45 +0800

    业务整合，给user类添加上下线和处理信息的API

--- user.go
+++ user.go
@@ -6,14 +6,17 @@ type User struct {
 	Addr net.Addr
 	C    chan string
 	Conn net.Conn
+
+	server *Server
 }
 
-func NewUser(conn net.Conn) *User {
+func NewUser(conn net.Conn, server *Server) *User {
 
 	user := &User{
-		Addr: conn.RemoteAddr(),
-		C:    make(chan string),
-		Conn: conn,
+		Addr:   conn.RemoteAddr(),
+		C:      make(chan string),
+		Conn:   conn,
+		server: server,
 	}
 	go user.CListener()
 
@@ -25,4 +28,28 @@ func (this *User) CListener() {
 		msg := <-this.C
 		this.Conn.Write([]byte(msg + "\n"))
 	}
+}
+
+// 用户的上线业务
+func (this *User) Online() {
+	this.server.Lock.Lock()
+	this.server.OnlineMap[this.Addr.String()] = this
+	this.server.Lock.Unlock()
+
+	//广播消息
+	this.server.BroadCast(this, "Online")
+}
+
+//用户的下线业务
+func (this *User) Offline() {
+	this.server.BroadCast(this, "["+this.Addr.String()+" Offline]")
+	//将该用户从Onlinemap中移除
+	this.server.Lock.Lock()
+	delete(this.server.OnlineMap, this.Addr.String())
+	this.server.Lock.Unlock()
+}
+
+// 处理用户消息的功能
+func (this *User) DoMessage(msg string) {
+	this.server.BroadCast(this, msg)
 }
